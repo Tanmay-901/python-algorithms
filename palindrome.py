c21b1ca20277ff710bebe377f22afa1942455d99
tejasvicsr1 <tejasvicsr1@gmail.com>
2020-10-16 23:53:51 +0530

    Added code to check if a given string is a palindrome or not.

---
+++ palindrome.py
@@ -0,0 +1,14 @@
+#python code which returns true if the given string is a palindrome else false.
+
+def isPalindrome(imput_string):
+	reverse_string = imput_string[::-1]
+
+	return imput_string == reverse_string
+
+
+checkstring = str(input("Enter the input string: "))
+
+if isPalindrome(checkstring):
+	print("Yes, it is a palindrome,")
+else:
+	print("No, it is not a palindrome.")
