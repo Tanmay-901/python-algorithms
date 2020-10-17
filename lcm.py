27f3ab4dedece9ea95679672673a84c89af13519
kelvinchanwh <35751887+kelvinchanwh@users.noreply.github.com>
2020-10-17 16:59:15 +0800

    Find lowest common denominator

---
+++ lcm.py
@@ -0,0 +1,20 @@
+from math import *
+
+#Calculates Lowest Common Multiple
+def compute_lcm(x, y):
+
+   # Select larger number
+   if x > y:
+       larger = x
+   else:
+       larger = y
+
+   while((larger % x != 0) or (larger % y != 0)):
+       larger += 1
+
+   return larger
+
+in1 = int(input("Insert 1st number: "))
+in2 = int(input("Insert 2nd number: "))
+
+print("LCM of %d and %d is"%(in1, in2), compute_lcm(in1, in2))
