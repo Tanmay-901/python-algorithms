bba93db6fe6ab29ec90be0fa4b198f2785bde2d9
kelvinchanwh <35751887+kelvinchanwh@users.noreply.github.com>
2020-10-18 04:08:35 +0800

    Added comments regarding how the loop functions

--- lcm.py
+++ lcm.py
@@ -3,16 +3,18 @@ from math import *
 #Calculates Lowest Common Multiple
 def compute_lcm(x, y):
 
-   # Select larger number
-   if x > y:
-       larger = x
-   else:
-       larger = y
+	# Select larger number
+	if x > y:
+	  	larger = x
+	else:
+		larger = y
 
-   while((larger % x != 0) or (larger % y != 0)):
-       larger += 1
+	# Checks if the larger value is divisible by both numbers
+	while((larger % x != 0) or (larger % y != 0)):
+			# If not divisible by either number, the larger value is incremented by 1
+	    larger += 1
 
-   return larger
+	return larger
 
 in1 = int(input("Insert 1st number: "))
 in2 = int(input("Insert 2nd number: "))
